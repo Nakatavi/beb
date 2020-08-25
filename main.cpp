675c20035e1fa3a80faba37dff4ef2ffee2217ee
BEB <beb4or@gmail.com>
2020-08-25 20:13:25 +0300

    changed main.cpp

--- main.cpp
+++ main.cpp
@@ -3,3 +3,13 @@ void main()
 	cout << "Hello world!" << endl;
 cout<<"bay"<<endl;
 }
+void main2()
+{
+	cout << "Hello world!" << endl;
+	cout << "bay" << endl;
+}
+void main3()
+{
+	cout << "Hello world!" << endl;
+	cout << "bay" << endl;
+}
